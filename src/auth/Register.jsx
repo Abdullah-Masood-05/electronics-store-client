cb01be4e4e58092f1814d3fe453e5cc184efb473
Abdullah Masood <mabd051104@gmail.com>
2026-02-07 14:21:42 +0500

    feat: implement registration functionality in Register component

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,9 +1,16 @@
-import React from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+
 const Register = () => {
-  return (
-    <div>
-    <p>Register</p>
-    </div>
-  )
-}
+  const handleRegister = async () => {
+    await createUserWithEmailAndPassword(
+      auth,
+      "test@example.com",
+      "Password123!"
+    );
+  };
+
+  return <button onClick={handleRegister}>Register</button>;
+};
+
 export default Register;
